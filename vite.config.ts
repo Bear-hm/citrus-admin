a8a0457ff00997078ea46196b1a7566372b2cce6
xiong <x1253780359@outlook.com>
2026-03-31 12:22:14 +0800

    style：修改主题色

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: xiong x1253780359@outlook.com
+ * @Date: 2025-10-10 08:42:06
+ * @LastEditors: xiong x1253780359@outlook.com
+ * @LastEditTime: 2026-03-31 11:55:54
+ * @FilePath: \citrus-frontendd:\FileData\school\citrus\citrus-admin\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE;
+ */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
